eb839eb18bc6e418ca5a04ca16db30a36181adb0
Ekaterina-T <tarakanova_ekaterina@hotmail.com>
2021-08-23 23:36:43 +0300

    review fixes

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -10,12 +10,15 @@ const FILE_TITLES_PATH = `./data/titles.txt`;
 const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
 const DEFAULT_COUNT = 1;
+const MAX_NUM_OF_POSTS = 1000;
 const FILE_NAME = `mocks.json`;
 
 const readDataFromFile = async (pathToFile) => {
   try {
     const data = await fs.readFile(pathToFile, `utf8`);
-    return data.trim().split(`\n`);
+    return data.trim().split(`\n`)
+    .map(el => el.trim())
+    .filter(el => el.length > 0);
   } catch (e) {
     console.error(chalk.red(`Can't read the file`));
     console.error(chalk.red(e));
@@ -43,32 +46,34 @@ const getPictureFileName = (number) => {
   return `item${id}.jpg`;
 };
 
-const generateOffers = (count, titles, sentences, categories) => (
-  Array(count).fill({}).map(() => ({
+const generateOffers = async (count) => {
+
+  const titles = await readDataFromFile(FILE_SENTENCES_PATH);
+  const sentences = await readDataFromFile(FILE_TITLES_PATH);
+  const categories = await readDataFromFile(FILE_CATEGORIES_PATH);
+
+  return Array(count).fill({}).map(() => ({
     type: OfferType[Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)]],
     title: titles[getRandomInt(0, titles.length - 1)],
     description: shuffle(sentences).slice(1, 5).join(` `),
     sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
     picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX)),
     category: [categories[getRandomInt(0, categories.length - 1)]],
-  }))
-);
+  }));
+
+};
 
 module.exports = {
   name: `--generate`,
   async run(count) {
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
 
-    if (count > 1000) {
-      console.error(chalk.red(`Не больше 1000 объявлений`));
+    if (count > MAX_NUM_OF_POSTS) {
+      console.error(chalk.red(`Не больше ${MAX_NUM_OF_POSTS} объявлений`));
       process.exit(ExitCode.failure);
     }
 
-    const titles = await readDataFromFile(FILE_SENTENCES_PATH);
-    const sentences = await readDataFromFile(FILE_TITLES_PATH);
-    const categories = await readDataFromFile(FILE_CATEGORIES_PATH);
-
-    const content = JSON.stringify(generateOffers(countOffer, titles, sentences, categories));
+    const content = JSON.stringify(await generateOffers(countOffer));
 
     try {
       await fs.writeFile(FILE_NAME, content);
